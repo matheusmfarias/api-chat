708e77a15c081a890e391592e0e92384c8795b31
Matheus Farias <matheus.2022005384@aluno.iffar.edu.br>
2024-09-04 00:37:21 -0300

    Finalizar painel da esquerda

--- controllers/JobSearchController.js
+++ controllers/JobSearchController.js
@@ -3,12 +3,27 @@ const Job = require('../models/Job');
 const getJobs = async (req, res) => {
     try {
         const keyword = req.query.keyword;
+        const filter = req.query.filter;
         const filters = { status: true }; // Apenas vagas ativas
 
+        // Filtrar por título da vaga (cargo)
         if (keyword) {
-            filters.title = { $regex: keyword, $options: 'i' };
+            filters.title = { $regex: keyword, $options: 'i' }; // Busca insensível a maiúsculas/minúsculas
         }
 
+        // Verifica se o filtro (cidade, estado, tipo ou modelo) está presente e aplica o filtro apropriado
+        if (filter) {
+            const filterRegex = { $regex: filter, $options: 'i' }; // Filtro insensível a maiúsculas e capturando qualquer parte da palavra
+
+            // Aplica o filtro diretamente em 'location' como string
+            filters.$or = [
+                { location: filterRegex },      // Filtrar por cidade e estado juntos
+                { modality: filterRegex },      // Filtrar por modalidade (Presencial, Remoto, Híbrido)
+                { type: filterRegex }           // Filtrar por tipo (Efetivo, Estágio, etc.)
+            ];
+        }
+
+        // Realiza a busca com os filtros aplicados
         const jobs = await Job.find(filters)
             .select('title location modality type publicationDate description salary pcd identifyCompany company')
             .populate({
