04f94af4ba2ffb77bb3a7fbfd2b79d1ecdd50ce1
Matheus Farias <matheus.2022005384@aluno.iffar.edu.br>
2024-09-28 15:20:10 -0300

    testes deploy 2

--- app.js
+++ app.js
@@ -7,17 +7,9 @@ const jobRoutes = require('./routes/jobRoutes');
 const jobSearchRoutes = require('./routes/jobSearchRoutes');
 const cors = require('cors');
 const app = express();
-/*
+
 const path = require('path');
 
-// Serve os arquivos estáticos do build do React
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Adicione esta rota para redirecionar todas as requisições para o index.html do React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-*/
 app.use(cors());
 app.use(express.json({ extended: false }));
 app.use('/api/auth', authRoutes);
@@ -26,5 +18,13 @@ app.use('/api/company', companyRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/jobsSearch', jobSearchRoutes);
+
+// Serve os arquivos estáticos do build do React
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Adicione esta rota para redirecionar todas as requisições para o index.html do React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
